2b9a97022a4c6f46584d70ee45708e56b6e2f9b3
lipegomes <fgdl.py91@gmail.com>
2021-02-21 19:55:38 -0300

    Add test for multiplication and division

--- test_calculator.py
+++ test_calculator.py
@@ -11,3 +11,9 @@ class TestCalculator:
 
     def test_subtraction(self):
         assert 4 == calculator.substract(10, 6)
+
+    def test_multiplication(self):
+        assert 10 == calculator.multiplication(1, 10)
+
+    def test_division(self):
+        assert 25 == calculator.division(100, 4)
